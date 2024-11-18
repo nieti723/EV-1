0c7fe331475a381ef8cb6edf2573279348e6a80e
nieti723 <juan.nieto-acosta@iesruizgijon.com>
2024-11-18 14:26:47 +0100

    update

--- src/tema8/funciones/FuncionesMatematicas1_a_19.java
+++ src/tema8/funciones/FuncionesMatematicas1_a_19.java
@@ -1,15 +1,95 @@
 package tema8.funciones;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class FuncionesMatematicas1_a_19 {
-    public static String esPar(int num){
-        if (num%2==0) {
-            return num + " es par";
+
+    public static void presentacion() throws InterruptedException, IOException{
+        @SuppressWarnings("resource")
+        Scanner s = new Scanner(System.in);
+        System.out.println("Bienvenido a la biblioteca de funciones");
+        System.out.println("Aquí se le mostrarán diferentes funcionalidades y usted");
+        System.out.println("escogerá la que necesite");
+        System.out.print("Pulse una tecla para continuar: ");
+        s.nextLine();
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+    }
+
+    public static int menu() throws InterruptedException, IOException{
+        @SuppressWarnings("resource")
+        Scanner s = new Scanner(System.in);
+        int opcion;
+        System.out.println("----------MENÚ----------");
+        System.out.println("1. Determina si un número introducido por teclado es capicúa o no (true/false)");
+        System.out.println("2. Determina si un número introducido por teclado es primo o no (true/false)");
+        System.out.println("3. Muestra el siguiente valor primo de uno introducido por teclado");
+        System.out.println("4. Muestra el resultado de la potencia de una base y un exponente introducidos por teclado");
+        System.out.println("5. Muestra la cantidad de dígitos de un número introducido por teclado");
+        System.out.println("6. Salir");
+        System.out.print("Escoja una opción: ");
+        opcion = s.nextInt();
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        return opcion;
+    }
+
+    public static boolean esCapicua(int num){
+        int volteado = 0, residuo = num;
+        for (int i = 1; i <= digitos(num); i++) {
+            volteado+= residuo%10*((int)(Math.pow(10, digitos(num)-i)));
+            residuo = residuo/10;
+        }
+        if (volteado==num) {
+            return true;
         }else{
-            return num + " no es par";
+            return false;
         }
     }
 
-    public static void mensaje(String palabra){
-        System.out.println("La palabra es: " + palabra);
+    public static boolean esPrimo(int num){
+        int div = 0;
+        for (int i = 1; i <= num/2; i++) {
+            if (num%i==0) {
+                div++;
+            }
+        }
+        if (div>1) {
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public static int siguientePrimo(int num){
+        boolean encontrado = false;
+        int siguiente = num;
+        do {
+            siguiente++;
+            if (esPrimo(siguiente)) {
+                encontrado = true;
+            }
+        } while (!encontrado);
+        return siguiente;
+    }
+
+    public static int potencia(int base, int exponente){
+        return (int)(Math.pow(base, exponente));
+    }
+
+    public static int potencia(int base, int exponente, int opcion){
+        int resultado = base;
+        for (int i = 0; i < exponente-1; i++) {
+            resultado*= base;
+        }
+        return resultado;
+    }
+
+    public static int digitos(int num){
+        int dig = 0;
+        while (num>0) {
+            num/=10;
+            dig++;
+        }
+        return dig;
     }
 }
--- src/tema8/main/Pruebas.java
+++ src/tema8/main/Pruebas.java
@@ -1,13 +1,83 @@
 package tema8.main;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 import tema8.funciones.FuncionesMatematicas1_a_19;
 
 public class Pruebas {
-    public static void main(String[] args) {
-        int numero = 20;
-        String palabra = "hola";
-        System.out.println();
-        System.out.println(FuncionesMatematicas1_a_19.esPar(numero));
-        FuncionesMatematicas1_a_19.mensaje(palabra);
+
+    @SuppressWarnings("resource")
+    public static void main(String[] args) throws InterruptedException, IOException {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        Scanner s = new Scanner(System.in);
+        int num, exponente, opcion, metodo;
+        FuncionesMatematicas1_a_19.presentacion();
+        do {
+            opcion = FuncionesMatematicas1_a_19.menu();
+            switch (opcion) {
+                case 1:
+                    System.out.print("Introduce el número a comprobar: ");
+                    num = s.nextInt();
+                    System.out.println(FuncionesMatematicas1_a_19.esCapicua(num));
+                    System.out.print("Pulse una tecla para continuar: ");
+                    s.nextLine();
+                    s.nextLine();
+                    new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+                    break;
+                case 2:
+                    System.out.print("Introduce el número a comprobar: ");
+                    num = s.nextInt();
+                    System.out.println(FuncionesMatematicas1_a_19.esPrimo(num));
+                    System.out.print("Pulse una tecla para continuar: ");
+                    s.nextLine();
+                    s.nextLine();
+                    new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+                    break;
+                case 3:
+                    System.out.print("Introduce el número: ");
+                    num = s.nextInt();
+                    System.out.println(FuncionesMatematicas1_a_19.siguientePrimo(num));
+                    System.out.print("Pulse una tecla para continuar: ");
+                    s.nextLine();
+                    s.nextLine();
+                    new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+                    break;
+                    case 4:
+                    System.out.print("Introduzca la base: ");
+                    num = s.nextInt();
+                    System.out.print("Introduzca el exponente: ");
+                    exponente = s.nextInt();
+                    System.out.print("¿Qué método desea utilizar? (1.Math.pow | 2.Bucle): ");
+                    do {
+                        metodo = s.nextInt();
+                        if (metodo>2 || metodo<1) {
+                            System.out.print("Valor no válido, introduzcalo de nuevo: ");
+                        }
+                    } while (metodo>2 || metodo<1);
+                    if (metodo==1) {
+                        System.out.println(FuncionesMatematicas1_a_19.potencia(num, exponente));
+                    }else{
+                        System.out.println(FuncionesMatematicas1_a_19.potencia(num, exponente,1));
+                    }
+                    System.out.print("Pulse una tecla para continuar: ");
+                    s.nextLine();
+                    s.nextLine();
+                    new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+                    break;
+                case 5:
+                    System.out.print("Introduce el número: ");
+                    num = s.nextInt();
+                    System.out.println("El número " + num + " tiene " + FuncionesMatematicas1_a_19.digitos(num) + " dígitos");
+                    System.out.print("Pulse una tecla para continuar: ");
+                    s.nextLine();
+                    s.nextLine();
+                    new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+                    break;
+                default:
+                System.out.println("La opción introducida no es válida");
+                    break;
+            }
+        } while (opcion!=6);
     }
 }
