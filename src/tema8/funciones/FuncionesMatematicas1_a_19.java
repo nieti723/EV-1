c9518c7956121df126732fa192a94219b331546e
nieti723 <juan.nieto-acosta@iesruizgijon.com>
2024-11-21 20:04:20 +0100

    terminado 1-10 tema8

--- src/tema8/funciones/FuncionesMatematicas1_a_19.java
+++ src/tema8/funciones/FuncionesMatematicas1_a_19.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class FuncionesMatematicas1_a_19 {
 
-    public static void presentacion() throws InterruptedException, IOException{
+    public static void presentacion() throws InterruptedException, IOException {
         @SuppressWarnings("resource")
         Scanner s = new Scanner(System.in);
         System.out.println("Bienvenido a la biblioteca de funciones");
@@ -16,7 +16,7 @@ public class FuncionesMatematicas1_a_19 {
         new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
     }
 
-    public static int menu() throws InterruptedException, IOException{
+    public static int menu() throws InterruptedException, IOException {
         @SuppressWarnings("resource")
         Scanner s = new Scanner(System.in);
         int opcion;
@@ -24,13 +24,16 @@ public class FuncionesMatematicas1_a_19 {
         System.out.println("1. Determina si un número introducido por teclado es capicúa o no (true/false)");
         System.out.println("2. Determina si un número introducido por teclado es primo o no (true/false)");
         System.out.println("3. Muestra el siguiente valor primo de uno introducido por teclado");
-        System.out.println("4. Muestra el resultado de la potencia de una base y un exponente introducidos por teclado");
+        System.out
+                .println("4. Muestra el resultado de la potencia de una base y un exponente introducidos por teclado");
         System.out.println("5. Muestra la cantidad de dígitos de un número introducido por teclado");
         System.out.println("6. Voltea un número introducido por teclado");
-        System.out.println("7. Salir");
-        System.out.println("8. Salir");
-        System.out.println("9. Salir");
-        System.out.println("10. Salir");
+        System.out.println(
+                "7. Devuelve el dígito que se encuentra en la posición n de un número introducida por teclado");
+        System.out
+                .println("8. Da la posición de la primer ocurrencia de un dígito introducido por teclado de un número");
+        System.out.println("9. Retira n dígitos por detrás de un número introducido por teclado");
+        System.out.println("10. Retira n dígitos por delante de un número introducido por teclado");
         System.out.println("11. Salir");
         System.out.println("12. Salir");
         System.out.println("13. Salir");
@@ -43,41 +46,41 @@ public class FuncionesMatematicas1_a_19 {
     }
 
     @SuppressWarnings("resource")
-    public static void salir() throws InterruptedException, IOException{
+    public static void salir() throws InterruptedException, IOException {
         Scanner s = new Scanner(System.in);
         System.out.print("Pulse una tecla para continuar: ");
         s.nextLine();
         new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
     }
 
-    public static boolean esCapicua(int num){
+    public static boolean esCapicua(int num) {
         int volteado = 0, residuo = num;
         for (int i = 1; i <= digitos(num); i++) {
-            volteado+= residuo%10*((int)(Math.pow(10, digitos(num)-i)));
-            residuo/= 10;
+            volteado += residuo % 10 * ((int) (Math.pow(10, digitos(num) - i)));
+            residuo /= 10;
         }
-        if (volteado==num) {
+        if (volteado == num) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
-    public static boolean esPrimo(int num){
+    public static boolean esPrimo(int num) {
         int div = 0;
-        for (int i = 1; i <= num/2; i++) {
-            if (num%i==0) {
+        for (int i = 1; i <= num / 2; i++) {
+            if (num % i == 0) {
                 div++;
             }
         }
-        if (div>1) {
+        if (div > 1) {
             return false;
-        }else{
+        } else {
             return true;
         }
     }
 
-    public static int siguientePrimo(int num){
+    public static int siguientePrimo(int num) {
         boolean encontrado = false;
         int siguiente = num;
         do {
@@ -89,39 +92,67 @@ public class FuncionesMatematicas1_a_19 {
         return siguiente;
     }
 
-    public static int potencia(int base, int exponente){
-        return (int)(Math.pow(base, exponente));
+    public static int potencia(int base, int exponente) {
+        return (int) (Math.pow(base, exponente));
     }
 
-    public static int potencia(int base, int exponente, int opcion){
+    public static int potencia(int base, int exponente, int opcion) {
         int resultado = base;
-        for (int i = 0; i < exponente-1; i++) {
-            resultado*= base;
+        for (int i = 0; i < exponente - 1; i++) {
+            resultado *= base;
         }
         return resultado;
     }
 
-    public static int digitos(int num){
+    public static int digitos(int num) {
         int dig = 0;
-        while (num>0) {
-            num/=10;
+        while (num > 0) {
+            num /= 10;
             dig++;
         }
         return dig;
     }
 
-    public static int volteaNumero(int num){
+    public static int volteaNumero(int num) {
         int volteado = 0, residuo = num;
         for (int i = 1; i <= digitos(num); i++) {
-            volteado+= residuo%10*((int)(Math.pow(10, digitos(num)-i)));
-            residuo/= 10;
+            volteado += residuo % 10 * ((int) (Math.pow(10, digitos(num) - i)));
+            residuo /= 10;
         }
         return volteado;
     }
 
-    public static int digitoN(int num, int n){
-        num = num%(int)(Math.pow(10, n));
-        num= num/(int)(Math.pow(10, digitos(num)-(n-1)));
+    public static int digitoN(int num, int n) {
+        num = num / (int) (Math.pow(10, digitos(num) - (n + 1)));
+        num = num % 10;
+        return num;
+    }
+
+    public static int posDig(int num, int dig) {
+        int primero = 0, posición = 0, aux = num;
+        boolean encontrado = false;
+        for (int i = 0; i < digitos(aux) && !encontrado; i++) {
+            primero = num / (int) (Math.pow(10, digitos(num) - 1));
+            num = num % (int) (Math.pow(10, digitos(aux) - (i + 1)));
+            if (primero == dig) {
+                encontrado = true;
+                posición = i;
+            }
+        }
+        if (encontrado) {
+            return posición;
+        } else {
+            return -1;
+        }
+    }
+
+    public static int quitaDetras(int num, int cantidad) {
+        num /= (int) (Math.pow(10, cantidad));
+        return num;
+    }
+
+    public static int quitaDelante(int num, int cantidad) {
+        num %= (int) (Math.pow(10, digitos(num) - cantidad));
         return num;
     }
 }
--- src/tema8/main/Pruebas.java
+++ src/tema8/main/Pruebas.java
@@ -11,13 +11,15 @@ public class Pruebas {
     public static void main(String[] args) throws InterruptedException, IOException {
         new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
         Scanner s = new Scanner(System.in);
-        int num, exponente, opcion, metodo;
+        int num = 0, opcion;
         boolean continuar = true;
         FuncionesMatematicas1_a_19.presentacion();
         do {
             opcion = FuncionesMatematicas1_a_19.menu();
-            System.out.print("Introduce el número: ");
-            num = s.nextInt();
+            if (opcion>0 && opcion<15) {
+                System.out.print("Introduce el número: ");
+                num = s.nextInt();  
+            }
             switch (opcion) {
                 case 1:
                     System.out.println(FuncionesMatematicas1_a_19.esCapicua(num));
@@ -33,8 +35,9 @@ public class Pruebas {
                     break;
                 case 4:
                     System.out.print("Introduzca el exponente: ");
-                    exponente = s.nextInt();
+                    int exponente = s.nextInt();
                     System.out.print("¿Qué método desea utilizar? (1.Math.pow | 2.Bucle): ");
+                    int metodo;
                     do {
                         metodo = s.nextInt();
                         if (metodo > 2 || metodo < 1) {
@@ -61,8 +64,31 @@ public class Pruebas {
                     System.out.print("Introduce la posición del dígito: ");
                     int posDig = s.nextInt();
                     System.out.println("El dígito que ocupa la posición " + posDig + " en el número " + num + " es " + FuncionesMatematicas1_a_19.digitoN(num, posDig));
+                    FuncionesMatematicas1_a_19.salir();
+                    break;
+                case 8:
+                    System.out.print("Introduce el dígito a buscar: ");
+                    int dig = s.nextInt();
+                    System.out.println("Posición: " + FuncionesMatematicas1_a_19.posDig(num, dig));
+                    FuncionesMatematicas1_a_19.salir();
+                    break;
+                case 9:
+                    System.out.print("Introduce la cantidad de dígitos a retirar: ");
+                    int cantidad = s.nextInt();
+                    System.out.println("Número original: " + num);
+                    System.out.println("Número con " + cantidad + " digito(s) retirado(s) " + FuncionesMatematicas1_a_19.quitaDetras(num, cantidad));
+                    FuncionesMatematicas1_a_19.salir();
+                    break;
+                case 10:
+                    System.out.print("Introduce la cantidad de dígitos a retirar: ");
+                    cantidad = s.nextInt();
+                    System.out.println("Número original: " + num);
+                    System.out.println("Número con " + cantidad + " digito(s) retirado(s) " + FuncionesMatematicas1_a_19.quitaDelante(num, cantidad));
+                    FuncionesMatematicas1_a_19.salir();
+                    break;
                 default:
                     continuar = false;
+                    System.out.println("Gracias por usar nuestro programa, vuelva pronto");
                     break;
             }
         } while (continuar);
