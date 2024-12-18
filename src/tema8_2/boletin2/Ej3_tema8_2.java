425373e3718ec8116b00f4db332f04f426e89556
nieti723 <juan.nieto-acosta@iesruizgijon.com>
2024-12-18 16:50:10 +0100

    holi

--- src/tema8_2/boletin2/Ej3_tema8_2.java
+++ src/tema8_2/boletin2/Ej3_tema8_2.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 
 //Contar cuantas palabras tiene una frase (hasta el primer punto)
 public class Ej3_tema8_2 {
+    @SuppressWarnings("resource")
     public static void main(String[] args) throws InterruptedException, IOException {
         new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
         Scanner s = new Scanner(System.in);
 
         // VARIABLES
         String frase = "";
-        int posPunto = 0;
         boolean valido = true;
 
         // PROGRAMA
@@ -20,18 +20,23 @@ public class Ej3_tema8_2 {
             valido = true;
             System.out.print("Introduzca la frase a comprobar: ");
             frase = s.nextLine();
-            if (frase.indexOf(".")==-1) {
-                System.out.println("La frase debe contener un punto para finalizar");
+            if (frase.charAt(frase.length()-2) == '.') {
                 valido = false;
             }
-        } while (!valido);
-        do {
-            valido = true;
-            if (frase.length()>frase.charAt(frase.indexOf("."))) {
-                if (frase.charAt(frase.indexOf(".")+1) == '.') {
-                    valido = false;
-                }
+            if (frase.charAt(frase.length()-1) != '.') {
+                valido = false;
+            }
+            if (!valido) {
+                System.out.println("La frase debe acabar en un punto");
             }
         } while (!valido);
+
+        frase = frase.trim();
+        String[] palabras = frase.split(" ");
+        if (palabras.length>1) {
+            System.out.println("La frase introducida tiene " + palabras.length + " palabras");
+        }else{
+            System.out.println("La frase introducida tiene " + palabras.length + " palabra");
+        }
     }
 }
